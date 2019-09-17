5f5b1f1efbed35732c67632c5810c5b2e28a8731
Rahul <rahul.rai@roadcast.in>
2019-09-17 11:52:48 +0530

    Request

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  baseUrl = 'http://fun2tech.xyz:5000/login';
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+    // this.httpClient.get(this.baseUrl + '/products').subscribe((res) => {
+    //   console.log(res);
+    // });
+
+    this.httpClient.post(this.baseUrl, {'username':'8199061908','password':'lakshya123'})
+      .subscribe(
+        (val) => {
+          console.log("POST call successful value returned in body",
+            val);
+        },
+        response => {
+          console.log("POST call in error", response);
+        },
+        () => {
+          console.log("The POST observable is now completed.");
+        });
   }
 
 }

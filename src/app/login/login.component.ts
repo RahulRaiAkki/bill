3fd9e50a1908ceb2ded2f2503e522c6e189361fd
Rahul <rahul.rai@roadcast.in>
2019-09-26 15:14:48 +0530

    jquery added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,23 +10,23 @@ export class LoginComponent implements OnInit {
   baseUrl = 'http://fun2tech.xyz:5000/login';
   constructor(private httpClient: HttpClient) { }
 
-  ngOnInit() {
-    // this.httpClient.get(this.baseUrl + '/products').subscribe((res) => {
-    //   console.log(res);
-    // });
+  ngOnInit() {/*
+    this.httpClient.get('http://fun2tech.xyz:5000/user').subscribe((res) => {
+      console.log(res);
+    });
 
-    this.httpClient.post(this.baseUrl, {'username':'8199061908','password':'lakshya123'})
+    this.httpClient.post(this.baseUrl, {username: '8199061908', password: 'lakshya123'})
       .subscribe(
         (val) => {
-          console.log("POST call successful value returned in body",
+          console.log('POST call successful value returned in body',
             val);
         },
         response => {
-          console.log("POST call in error", response);
+          console.log('POST call in error', response);
         },
         () => {
-          console.log("The POST observable is now completed.");
+          console.log('The POST observable is now completed.');
         });
-  }
+  */}
 
 }
